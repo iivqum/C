9a5419ec5cc0e325464df77b3b3e29622541a21a
iivqum <59771955+iivqum@users.noreply.github.com>
2020-06-22 13:39:27 +0100

    Update raytrace.c

--- raytrace.c
+++ raytrace.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 #include <png.h>
 
-int SCNW = 256;
-int SCNH = 256;
+int SCNW = 512;
+int SCNH = 512;
 
+float EPSL = 1e-3;
+float PI = 3.1415;
+
+typedef struct Color{unsigned char r,g,b;}Color;
 typedef struct P3D{float x,y,z;}P3D;
 typedef struct Ray{P3D p,d;}Ray;
-typedef struct HitRecord{float t;P3D p,n;}HitRecord;
+typedef struct HitRecord{float t;P3D p,n;int hit;}HitRecord;
 typedef struct Sphere{P3D p;float r;}Sphere;
 
 void PSet(P3D *p,float x,float y,float z){p->x=x;p->y=y;p->z=z;}
@@ -20,7 +25,54 @@ float PDot(P3D *p0,P3D *p1){
 }
 void PScl(P3D *p0,float s){p0->x*=s;p0->y*=s;p0->z*=s;}
 
-int RayHitSphere(Ray *r,Sphere *s)
+void ErrNoMem(void){perror("NOMEM");abort();}
+
+float RandomFrac(void){
+	return (float)rand()/(float)(RAND_MAX+1);
+}
+
+float RandomFracSign(void){
+	return 2*RandomFrac()-1;
+}
+
+void RandomInSphere(P3D *p)
+{
+	float t = RandomFrac()*2*PI;
+	float z = RandomFracSign();
+	float r = sqrtf(1-z*z);
+	PSet(p,r*cosf(t),r*sinf(t),z);
+}
+
+P3D *P3DInit(float x,float y,float z){
+	P3D *ptr = malloc(sizeof(P3D));
+	if (ptr==NULL)ErrNoMem();
+	PSet(ptr,x,y,z);
+	return ptr;
+}
+
+Color *ColorInit(unsigned char r,unsigned char g,unsigned char b){
+	Color *ptr = malloc(sizeof(Color));
+	if (ptr==NULL)ErrNoMem();
+	ptr->r = r;
+	ptr->g = g;
+	ptr->b = b;
+	return ptr;
+}
+
+Sphere *SphereInit(float x,float y,float z,float r)
+{
+	struct Sphere *sp = malloc(sizeof(struct Sphere));
+	if (sp==NULL)ErrNoMem();
+	PSet(&sp->p,x,y,z);
+	sp->r = r;
+	return sp;
+}
+
+#define NSPHERES 2
+
+Sphere spherelist[NSPHERES];
+
+float RayHitSphere(Ray *r,Sphere *s)
 {
 	P3D oc;
 	PSetP(&oc,&r->p);
@@ -33,19 +85,58 @@ int RayHitSphere(Ray *r,Sphere *s)
 		return 0;
 	float d2 = sqrtf(d);
 	float root = (-b-d2)/(2*a);
-	if (root>0)return 1;
+	if (root>EPSL)return root;
 	root = (-b+d2)/(2*a);
-	if (root>0)return 1;
-	return 0;
+	if (root>EPSL)return root;
+	return -1;
 }
 
-struct Sphere *SphereInit(float x,float y,float z,float r)
+int GetNearestHit(Ray *r,HitRecord *rec)
 {
-	struct Sphere *sp = malloc(sizeof(struct Sphere));
-	if (sp==NULL)return NULL;
-	PSet(&sp->p,x,y,z);
-	sp->r = r;
-	return sp;
+	float t = FLT_MAX;
+	Sphere *s = NULL;
+	for (int i=0;i<NSPHERES;i++){
+		float t2 = RayHitSphere(r,spherelist+i);
+		if (t2>0&&t2<t){
+			t = t2;
+			s = spherelist+i;
+		}
+	}
+	if (s==NULL)
+		return 0;
+	PSetP(&rec->p,&r->d);
+	PScl(&rec->p,rec->t);
+	PAdd(&rec->p,&r->p);
+	//sphere normal (p-c)/||(p-c)||
+	PSetP(&rec->n,&rec->p);
+	PSub(&rec->n,&s->p);
+	PScl(&rec->n,PDot(&rec->n,&rec->n));
+	return 1;
+}
+
+Color *TraceRay(Ray *r,int depth)
+{
+	if (depth<=0)return ColorInit(0,0,0);
+	HitRecord rec;
+	if (!GetNearestHit(r,&rec))
+		return ColorInit(255,255,255);
+	P3D d,s;
+	RandomInSphere(&d);
+	PAdd(&d,&rec.n);
+
+	PSetP(&s,&rec.p);
+	PAdd(&s,&d);
+
+	PSub(&s,&rec.p);
+	Ray r2;
+	PSetP(&r2.p,&rec.p);
+	PSetP(&r2.d,&s);
+	Color *col = TraceRay(&r2,depth-1);
+	col->r = (float)col->r*(float)0.5;
+	col->g = (float)col->g*0.5;
+	col->b = (float)col->b*0.5;
+	printf("%d,%d,%d\n",col->r,col->g,col->b);
+	return col;
 }
 
 int main(int argc, char *argv[])
@@ -76,7 +167,12 @@ int main(int argc, char *argv[])
 	);
 	png_write_info(png_ptr,info_ptr);
 
-	Sphere *s = SphereInit(0,0,-20,10);
+	PSet(&spherelist[0].p,0,0,-30);
+	spherelist[0].r = 10;
+
+	PSet(&spherelist[1].p,0,-1010,-30);
+	spherelist[1].r = 1000;
+
 	Ray r;
 	PSet(&r.p,0,0,0);
 	float ar = SCNW/SCNH;
@@ -86,15 +182,12 @@ int main(int argc, char *argv[])
 			float u = (2*((float)j/((float)SCNW-1))-1)*ar;
 			float v = -(2*((float)i/((float)SCNH-1))-1);
 			PSet(&r.d,u,v,-1);
-			if (RayHitSphere(&r,s)){
-				row[j*3] = 255;
-				row[j*3+1] = 255;
-				row[j*3+2] = 255;
-			}else{
-				row[j*3] = 0;
-				row[j*3+1] = 0;
-				row[j*3+2] = 0;
-			}
+			Color *col = TraceRay(&r,50);
+			//printf("%f,%f,%f\n",col->x,col->y,col->z);
+			row[j*3] = (png_byte)col->r;
+			row[j*3+1] = (png_byte)col->g;
+			row[j*3+2] = (png_byte)col->b;
+			free(col);
 		}
 		png_write_row(png_ptr,(png_bytep)row);
 	}
